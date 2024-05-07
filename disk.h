70f1463059e63e46df08107c146dd513d69ec082
yang-ng <131299315+yang-ng@users.noreply.github.com>
2024-05-08 00:50:00 +0800

    Update disk.h

--- disk.h
+++ disk.h
@@ -1,1 +1,17 @@
-// manage the physical disk
+#ifndef DISK_H
+#define DISK_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#define BLOCK_SIZE 512
+#define BLOCKS_PER_CLUSTER 1
+
+// function prototypes
+void disk_init(const char *disk_image_path);
+void* disk_read_cluster(int clusterNumber);
+void disk_write_cluster(int clusterNumber, const void* data);
+int cluster_to_sector(int clusterNumber);
+void disk_cleanup();
+
+#endif /* DISK_H */
